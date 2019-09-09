1bd2c415b587fdc37988370c139a576aca6167e0
xy <yxyyysdy@gmail.com>
2019-09-09 13:39:09 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@
 
 echo Let us pretend that this is building something ... and takes 21 seconds
 
-FOR /l %%A in (1, 1, 21) DO (
-  ECHO Building [[01;31m %%A of 21 [00m]
+FOR /l %%A in (1, 1, 20) DO (
+  ECHO Building [[01;31m %%A of 20 [00m]
   PING 192.0.2.1 -n 1 -w 1000 >NUL
 )
 
